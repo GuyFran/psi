bc5630a8269ce13f36627277a14724539075ae38
Guynemer FRANCLIN <guynemerp@free.fr>
2018-02-05 19:29:54 +0800

    adding custom point annotation class

---
+++ PSI/Custom/MyAnnotation.swift
@@ -0,0 +1,15 @@
+//
+//  MyAnnotation.swift
+//  PSI
+//
+//  Created by Guynemer on 5/2/18.
+//  Copyright © 2018 SP Test. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+
+class MyAnnotation: MKPointAnnotation {
+    var region:String?
+}
