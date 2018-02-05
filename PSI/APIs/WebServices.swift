8c65d17b6441679ddf1926c853ef6dda393b2347
Guynemer FRANCLIN <guynemerp@free.fr>
2018-02-05 14:26:32 +0800

    Addind classes for webservices and datamodeling

---
+++ PSI/APIs/WebServices.swift
@@ -0,0 +1,19 @@
+//
+//  WebServices.swift
+//  PSI
+//
+//  Created by Guynemer on 5/2/18.
+//  Copyright © 2018 SP Test. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+import Alamofire_SwiftyJSON
+
+fileprivate let kAPI_KEY:String = "iCc0KKrOCQ3jrD2BlPWeyAd0sJgx8iPw"
+fileprivate let kBASE_URL:String = "https://api.data.gov.sg/v1/environment/psi"
+
+class WebServices {
+    
+}
---
+++ PSI/DataModeling/DataHandling.swift
@@ -0,0 +1,9 @@
+//
+//  DataHandling.swift
+//  PSI
+//
+//  Created by Guynemer on 5/2/18.
+//  Copyright © 2018 SP Test. All rights reserved.
+//
+
+import Foundation
