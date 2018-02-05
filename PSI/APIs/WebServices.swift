7b7a0f7c2cfe9e82b4cd439fe81efb32c68df631
Guynemer FRANCLIN <guynemerp@free.fr>
2018-02-05 14:43:43 +0800

    basics for psi api

--- PSI/APIs/WebServices.swift
+++ PSI/APIs/WebServices.swift
@@ -16,7 +16,18 @@ fileprivate let kBASE_URL:String = "https://api.data.gov.sg/v1/environment/psi"
 
 class WebServices {
     
-    
+    //fetch PSI Json and gives back the array of readings and the regions metadata
+    //date in format YYYY-MM-DD
+    func getPSI(date:String?) {
+        
+        
+        //target URL
+        guard let apiURL = URL(string: kBASE_URL) else {
+            log.error("getPSI API Call failed : wrong base url")
+            postNotification(name: apiCallFailed)
+            return
+        }
+    }
     
     
     //neet to provide API KEY
